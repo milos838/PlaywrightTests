35a53fe4fd06a50724b41132d00634200851303d
Milos Jancic <milos.jancic83gmail.com>
2025-05-27 09:31:51 +0200

    Removing comments from E2E test

--- tests/E2E_Test.spec.js
+++ tests/E2E_Test.spec.js
@@ -4,35 +4,34 @@ const {DashboardPage} = require("../Pages/DashboardPage");
 const {CartPage} = require("../Pages/CartPage");
 const {OrderPage} = require("../Pages/OrderPage");
 
-//importing json data file
 const testData = require("../Data/testData.json");
 
 test('E2E test', async ({ page }) => {
 
-//deklarisanje stranice koja povlaci funkciju    
+  
 const loginPage = new LoginPage1(page);
-//funkcije iz LoginPage1 
+
 await loginPage.goTo(testData.url);
 await loginPage.logIn(testData.username,testData.password);
 await loginPage.validateNavigation(testData.landingPage);
 
 
-//deklarisanje stranice Dashboard
+
 const dashboardPage = new DashboardPage(page);
-//funkcije iz dashboard-a
+
 await dashboardPage.addToCard(testData.product);
 await dashboardPage.openCart();
 
 
-//deklarisanje stranice Cart
+
 const cartPage = new CartPage(page);
-//funkcije iz cart-a
+
 await cartPage.checkTotal(testData.cena);
 await cartPage.checkOut();
 
-//deklarisanje order stranice
+
 const orderPage = new OrderPage(page);
-//funkcije iz ordera
+
 await orderPage.selectCountry();
 await orderPage.placeOrder();
 
