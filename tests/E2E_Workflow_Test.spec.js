4d5ba5a23fe97095003d09b95043cf6328b78f73
Milos Jancic <milos.jancic83gmail.com>
2025-06-17 19:27:41 +0200

    Steps updates

--- tests/E2E_Workflow_Test.spec.js
+++ tests/E2E_Workflow_Test.spec.js
@@ -6,32 +6,27 @@ const {OrderPage} = require("../Pages/OrderPage");
 
 const testData = require("../Data/testData.json");
 
-test('E2E test', async ({ page }) => {
+test('E2E application workflow test', async ({ page }) => {
 
-  
+//Step 1. Login to the application
 const loginPage = new LoginPage1(page);
-
 await loginPage.goTo(testData.url);
 await loginPage.logIn(testData.username,testData.password);
 await loginPage.validateNavigation(testData.landingPage);
 
 
-
+//Step 2. Add a product to the cart
 const dashboardPage = new DashboardPage(page);
-
 await dashboardPage.addToCard(testData.product);
 await dashboardPage.openCart();
 
-
-
+//Step 3. Cart validation and checkout
 const cartPage = new CartPage(page);
-
 await cartPage.checkTotal(testData.cena);
 await cartPage.checkOut();
 
-
+//Step 4. Placing the order
 const orderPage = new OrderPage(page);
-
 await orderPage.selectCountry();
 await orderPage.placeOrder();
 
